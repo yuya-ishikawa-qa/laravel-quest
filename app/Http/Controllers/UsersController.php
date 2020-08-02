f805db8111e1448247d795eb14040e091cbe937b
Takano <anpanmannoatarashiikao@gmail.com>
2020-08-02 17:20:48 +0000

    add:LaravelQuest4章作成

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\User;
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        $users = User::orderBy('id', 'desc')->paginate(9);
+        
+        return view('welcome', [
+            'users' => $users,
+        ]);
+    }
+    
+    public function show($id)
+    {
+     $user = User::find($id);
+     $movies = $user->movies()->OrderBy('id', 'desc')->paginate(9);
+     
+     $data=[
+         'user' => $user,
+         'movies' => $movies,
+         ];
+         
+         $data += $this->counts($user);
+         
+         return view('users.show',$data);
+    }
+}
