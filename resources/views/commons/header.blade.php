9bae1c20d5eb20790863cf2d8ac13004595e6097
shuhei <a.shu.eishisassou@gmail.com>
2020-08-09 12:29:09 +0000

    laravel-quest３章　終了しました

---
+++ resources/views/commons/header.blade.php
@@ -0,0 +1,28 @@
+
+<header class="mb-5">
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+        <a class="navbar-brand" href="/">YouTube-Curation</a>
+
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li class="nav-item">{!! link_to_route('signup', '新規ユーザ登録', [], ['class' => 'nav-link']) !!}</li>
+              <li class="nav-item"><a href="" class="nav-link">ログイン</a></li>
+            </ul>
+        </div>
+
+    </nav>
+
+</header>
+
+
+        
+           
+            
+            
