dd66e30b247d0afb437704b369cadcd9e03ec179
shuhei <a.shu.eishisassou@gmail.com>
2020-08-13 13:15:32 +0000

    laravel-quest 4章終了しました

---
+++ resources/views/movies/movies.blade.php
@@ -0,0 +1,45 @@
+<div class="movies row mt-5 text-center">
+    
+    @foreach ($movies as $key => $movie)
+
+        @if($loop->iteration % 3 == 1 && $loop->iteration != 1)
+    
+            @php
+                echo '</div><div class="row text-center mt-3">';
+            @endphp
+        
+        @endif
+    
+            <div class="col-lg-4 mb-5">
+
+                <div class="movie text-left d-inline-block">
+
+                    <div>
+                       @if($movie)
+                            <iframe width="290" height="163.125" src="{{ 'https://www.youtube.com/embed/'.$movie->url }}?controls=1&loop=1&playlist={{ $movie->url }}" frameborder="0"></iframe>
+                        @else
+                            <iframe width="290" height="163.125" src="https://www.youtube.com/embed/" frameborder="0"></iframe>
+                        @endif
+                    </div>
+                    
+                    <p>
+                        @if(isset($movie->comment))
+                            {{ $movie->comment }}
+                        @endif
+                    </p>
+                    
+                    @if(Auth::id() == $movie->user_id)
+                        {!! Form::open(['route' => ['movies.destroy', $movie->id], 'method' => 'delete']) !!}
+                            {!! Form::submit('この動画を削除する？', ['class' => 'button btn btn-danger']) !!}
+                        {!! Form::close() !!}
+                    @endif
+
+                </div>
+                
+            </div>
+
+    @endforeach
+
+</div>
+
+{{ $movies->render('pagination::bootstrap-4') }}
