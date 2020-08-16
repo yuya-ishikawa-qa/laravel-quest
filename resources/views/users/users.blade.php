aa9e29bab35578def295023fc2a7eb7f2b74b5b0
shuhei <a.shu.eishisassou@gmail.com>
2020-08-16 19:49:43 +0000

    larave-quest５章終了しました

--- resources/views/users/users.blade.php
+++ resources/views/users/users.blade.php
@@ -37,6 +37,8 @@
                                {{ $movie->comment }}
                         @endif
                     </p>
+                    
+                        @include('follow.follow_button',['user'=>$user])
 
                 </div>
                 
