c3cdf2c1c9d9701c61a04e667a8b55c411ecfb44
tadano <tadanokenji10@gmail.com>
2020-08-26 14:17:39 +0000

    3章（確認用）

---
+++ resources/views/commons/footer.blade.php
@@ -0,0 +1,6 @@
+<footer>
+    <nav class="navber navbar-light bg-light justify-content-center">
+            <div class="navbsr-brand m-0">© QoestAcademia, All rights reserved.</div>
+    </nav>
+    
+</footer>
---
+++ resources/views/commons/header.blade.php
@@ -0,0 +1,21 @@
+<header class="mb-5">
+    
+    <nav class="navbar" navbar-expand-ig navbar-light bg-light">
+    
+        <a class="navbar-brand" href="/">Youtube-Curation</a>
+        
+        <botton type="botton" class="navbar-toggler" data-toggler="collapse" data=target="nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </botton>
+        
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                <li class="nav-item"><a href="" class="nav-link">新規ユーザ登録</a></li>
+                <li class="nav-item"><a href="" class="nav-link">ログイン</a></li>
+            </ul>
+        </div>
+    
+    </nav>
+    
+</header>
