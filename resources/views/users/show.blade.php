7cccef83b6fe1d0efc08d88e0405073fcdd8fce9
tadano <tadanokenji10@gmail.com>
2020-09-03 06:48:39 +0000

    5章完了

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,16 +2,9 @@
 
 @section('content')
 
-<h1>{{ $user->channel }}</h1>
-<h1 class="text-right">{{ $user->name }}</h1>
+    @include('users.tabs', ['user'=>$user])
 
-<ul class="nav nav-tabs nav-justified mt-5 mb-2">
-        <li class="nav-item nav-link {{ Request::is('users/' . $user->id) ? 'active' : '' }}"><a href="{{ route('users.show',['id'=>$user->id]) }}">動 画<br><div class="badge badge-secondary">{{ $count_movies }}</div></a></li>
-        <li class="nav-item nav-link"><a href="" class="">フォロワー<br><div class="badge badge-secondary"></div></a></li>
-        <li class="nav-item nav-link"><a href="" class="">フォロー中<br><div class="badge badge-secondary"></div></a></li>
-</ul>
-
-@include('movies.movies', ['movies' => $movies])
+    @include('movies.movies', ['movies' => $movies])
 
 @if(Auth::id() == $user->id)
 
